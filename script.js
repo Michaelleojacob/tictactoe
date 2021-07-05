d228a7fc96a8d282a40605288dd440dfc14a214c
Michael <michaelleojacob@gmail.com>
2021-07-05 14:47:20 -0700

    stuck on trying to make a function to decide whose turn it is lol. a state type solution just isn't working in an iife. Which I remember reading about. So lets do some google foo

--- script.js
+++ script.js
@@ -5,9 +5,11 @@
         init: function(){
             this.cacheDom();
             this.displayGrid();
+            this.playerTurn();
         },
         cacheDom: function(){
             this.el = document.querySelector("#gameboard");
+            this.error = document.querySelector(".displayError");
         },
         displayGrid: function(){
             let numOfGrids = 0;
@@ -19,10 +21,30 @@
                 numOfGrids++;
             };
         },
-        player1: function(){
+        renderPlayerMarkerOnClick: function(player){
             this.el.addEventListener("click", function(e){
-                console.log(e);
+                if(e.target.innerHTML !== "")return;
+                if(e.target.className.includes("tile")){
+                    e.target.innerHTML = player;
+                }
             })
+        },
+        player1: function(){
+            this.renderPlayerMarkerOnClick("X");
+        },
+        player2: function(){
+            this.renderPlayerMarkerOnClick("O");
+        },
+        playerTurn: function(){
+            let lastPlayer= 1;
+            if(lastPlayer === 1){
+                this.player1();
+                lastPlayer = 0;
+            }
+            else{
+                this.player2();
+                lastPlayer = 1;
+            }
         },
     }
     game.init();
