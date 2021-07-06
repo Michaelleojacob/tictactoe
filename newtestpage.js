8f299bad8c9560c59278f4838275e5b7856a0936
Michael <michaelleojacob@gmail.com>
2021-07-06 10:52:16 -0700

    fixed a bug with how lastplayer was being changed. Was allowing for an illegal move to skip a player. So one player could cheese the game

--- newtestpage.js
+++ newtestpage.js
@@ -4,16 +4,16 @@ const board = [];
 let lastPlayer = "O";
 let markedTileCounter = 0;
 let gameOver = false;
-const winningCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-];
+// const winningCombinations = [
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     [0,4,8],
+//     [2,4,6],
+// ];
 
 function makeGrid(){
     let numOfGrids = 0;
@@ -38,34 +38,42 @@ function checkForWinner(player){
     }
     if(board[3] === player && board[4] === player && board[5] === player){
         console.log("condition 1 ran");
+        error.textContent = `${player} won`;
         return gameOver = true;
     }
     if(board[6] === player && board[7] === player && board[8] === player){
         console.log("condition 2 ran");
+        error.textContent = `${player} won`;
         return gameOver = true;
     }
     if(board[0] === player && board[3] === player && board[6] === player){
         console.log("condition 3 ran");
+        error.textContent = `${player} won`;
         return gameOver = true;
     }
     if(board[1] === player && board[4] === player && board[7] === player){
         console.log("condition 4 ran");
+        error.textContent = `${player} won`;
         return gameOver = true;
     }
     if(board[2] === player && board[5] === player && board[8] === player){
         console.log("condition 5 ran");
+        error.textContent = `${player} won`;
         return gameOver = true;
     }
     if(board[0] === player && board[4] === player && board[8] === player){
         console.log("condition 6 ran");
+        error.textContent = `${player} won`;
         return gameOver = true;
     }
     if(board[2] === player && board[4] === player && board[6] === player){
         console.log("condition 7 ran");
+        error.textContent = `${player} won`;
         return gameOver = true;
     }
     if(board.forEach(element => element === "X" || "O")){
         console.log("condition 8 - tie ran");
+        error.textContent = `tie game`;
         return gameOver = true;
     }
 }
@@ -82,6 +90,7 @@ function renderPlayerMarkerOnClick(player){
             e.target.innerHTML = player;
             error.textContent = "";
             board[board.indexOf(e.target)] = player;
+            lastPlayer = player;
             checkForWinner(player)
             testing();      
         }
@@ -91,17 +100,9 @@ function renderPlayerMarkerOnClick(player){
 function testing (){
     if(lastPlayer === "O"){
         player1();
-        console.log(board);
-        // checkForWinner("X");
-        // renderPlayerMarkerOnClick("X");
-        // lastPlayer = "X";
     }
     else{
         player2();
-        console.log(board)
-        // checkForWinner("O");
-        // renderPlayerMarkerOnClick("O");
-        // lastPlayer = "O";
     }
 }
 testing();
@@ -109,11 +110,9 @@ testing();
 
 function player1(){
     renderPlayerMarkerOnClick("X");
-    lastPlayer = "X";
 }
 
 function player2(){
     renderPlayerMarkerOnClick("O");
-    lastPlayer = "O";
 }
 
