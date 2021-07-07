a4ef0868ee59e65c7e269937ebd6f2961475a52b
Michael <michaelleojacob@gmail.com>
2021-07-07 15:50:37 -0700

    solved a small issue with singleEventListener not grabbing the dropdown value properly

--- ghostship.js
+++ ghostship.js
@@ -3,11 +3,13 @@
     const game = {
         board: [],
         lastPlayer: "O",
+        playerOrBot: "player",
         gameOver: false,
         init: function(){
             this.cacheDom();
             this.displayGrid();
             this.singleEventListener();
+            this.getDropDownValue();
         },
         cacheDom: function(){
             this.wrapper = document.querySelector(".wrapper");
@@ -16,6 +18,16 @@
             this.error = this.wrapper.querySelector(".displayError");
             this.restartbtn = this.wrapper.querySelector(".restart");
             this.turnDisplay = this.wrapper.querySelector(".turn");
+            this.selectValue = this.wrapper.querySelector(".playerOrBot");
+        },
+        getDropDownValue:function(){
+            let playerOrBot = this.selectValue.value;
+            game.wrapper.addEventListener("change", function(e){
+                playerOrBot = e.target.value;
+                game.handleRestartLogic();
+                return playerOrBot;
+            });
+            return playerOrBot;
         },
         displayGrid: function(){
             let numOfGrids = 0;
@@ -81,6 +93,7 @@
         },
         singleEventListener: function(){
             game.wrapper.addEventListener("click", function(e){
+                const playerOrBot = game.selectValue.value;
                 const event = e;
                 if(e.target.className.includes("restart")){
                     game.handleRestartLogic();
@@ -91,14 +104,21 @@
                         game.error.innerText = "that tile is already taken"
                         return;
                     };
-                    game.renderPlayerMarkerOnClick(event);
+                    if(playerOrBot === "player"){
+                        console.log(`${game.playerOrBot} should be player`)
+                        game.playerVsPlayer(event);
+                    }
+                    else if(playerOrBot === "randoRandy"){
+                        console.log(`${game.playerOrBot} should be rando`)
+                        game.playerVsRandoRandy(event);
+                    }
                     game.error.innerText = "";
                     game.checkForWinner("X");
                     game.checkForWinner("O");
                 }
             })
         },
-        renderPlayerMarkerOnClick: function(e){
+        playerVsPlayer: function(e){
             if(game.lastPlayer === "O"){
                 e.target.innerText = "X";
                 game.board[game.board.indexOf(e.target)] = "X";
@@ -112,6 +132,19 @@
                 return game.lastPlayer = "O";
             }
         },
+        playerVsRandoRandy: function(e){
+            if(game.lastPlayer === "O"){
+                e.target.innerText = "X";
+                game.board[game.board.indexOf(e.target)] = "X";
+                game.turnDisplay.innerText = "Turn: player O"
+                game.lastPlayer = "X";
+
+                function getRandomInt(max){
+                    return Math.floor(Math.random() * max);
+                }
+                console.log(getRandomInt(9));
+            }
+        }
     }
     game.init();
 })();
