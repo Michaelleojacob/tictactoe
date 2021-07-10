43168190d72c984d6b26d38554431b6bbd300144
Michael <michaelleojacob@gmail.com>
2021-07-10 09:10:27 -0700

    bot now displayed a result in on the screen. I solved for how to connect the bot pick to the div.innerText on the screen. There seems to be an issue with the bot picking tiles that should otherwise not be playable

--- attempingBotOutsideiife.js
+++ attempingBotOutsideiife.js
@@ -12,6 +12,7 @@ board.addEventListener("click", function(e){
     if(e.target.className.includes("tile")){
         if(e.target.innerText !== "") return;
         let currTarget = e.target.classList[1];
+        console.log(e.target.classList[1])
         currTarget = parseInt(currTarget);
         arr[arr.indexOf(currTarget)] = "X";
         delete botarr[botarr.indexOf(currTarget)];
@@ -19,14 +20,25 @@ board.addEventListener("click", function(e){
         botarr = botarr.filter(Number.isFinite);
         e.target.innerText = "X";
 
-        console.log(arr);
-        console.log(botarr);
+        // console.log(arr);
+        // console.log(botarr);
 
         function getRandomInt(max){
             return Math.floor(Math.random() * max);
         }
         let randomNumFromBotarr = getRandomInt(botarr.length);
         console.log(randomNumFromBotarr);
-
+        let boxes = document.querySelectorAll(".tile");
+        let boxArr = Array.from(boxes);
+        // console.log(boxArr[0].classList[1]);
+        for(let i=0; i<boxArr.length; i++){
+            // console.log(boxArr[i].classList[1])
+            if(boxArr[i].classList[1].includes(randomNumFromBotarr)){
+                boxArr[i].innerText = "O";
+                delete botarr[botarr.indexOf(randomNumFromBotarr)];
+                botarr = botarr.filter(Number.isFinite);
+                arr[arr.indexOf(randomNumFromBotarr)] = "O";
+            }
+        }
     }
 })
